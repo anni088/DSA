4a48e4019e87e4b906faeb2ec37cc091f789f504
anni088 <asthakur285@gmail.com>
2025-09-23 10:13:16 +0530

    Create GFG Left View of Binary Tree.cpp

---
+++ GFG Left View of Binary Tree.cpp
@@ -0,0 +1,54 @@
+/*
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+ */
+
+class Solution {
+  public:
+    vector<int> leftView(Node *root) {
+        // code here
+        vector<int> ans;
+        if(!root)
+        return ans;
+        
+        map<int, int> m;
+        queue<pair<Node*, int>> q;
+        q.push(make_pair(root, 0));
+        
+        while(!q.empty())
+        {
+            pair<Node*, int> temp = q.front();
+            q.pop();
+            
+            Node* fNode = temp.first;
+            int lvl = temp.second;
+            
+            if(!m[lvl])
+            m[lvl] = fNode -> data;
+            
+            if(fNode -> left)
+            q.push(make_pair(fNode->left, lvl+1));
+            
+            if(fNode -> right)
+            q.push(make_pair(fNode->right, lvl+1));
+        }
+        
+        for(auto val : m)
+        {
+            ans.push_back(val.second);
+        }
+        
+        return ans;
+    }
+};
