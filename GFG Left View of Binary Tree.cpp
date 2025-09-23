26456ac1576a7dd1a3661183ec57d7d591ca9d88
anni088 <asthakur285@gmail.com>
2025-09-23 11:25:04 +0530

    Update with recursion GFG Left View of Binary Tree.cpp

--- GFG Left View of Binary Tree.cpp
+++ GFG Left View of Binary Tree.cpp
@@ -16,39 +16,68 @@ public:
 
 class Solution {
   public:
+
+void solve(Node* root, vector<int>& ans, int lvl)
+    {
+        
+        if(!root)
+        return ;
+        
+        if(lvl == ans.size())
+        ans.push_back(root -> data);
+        
+        solve(root->left, ans, lvl+1);
+        solve(root->right, ans, lvl+1);
+    }
+  public:
     vector<int> leftView(Node *root) {
         // code here
+        
         vector<int> ans;
         if(!root)
         return ans;
         
-        map<int, int> m;
-        queue<pair<Node*, int>> q;
-        q.push(make_pair(root, 0));
-        
-        while(!q.empty())
-        {
-            pair<Node*, int> temp = q.front();
-            q.pop();
-            
-            Node* fNode = temp.first;
-            int lvl = temp.second;
-            
-            if(m.find(lvl) == m.end())
-            m[lvl] = fNode -> data;
-            
-            if(fNode -> left)
-            q.push(make_pair(fNode->left, lvl+1));
-            
-            if(fNode -> right)
-            q.push(make_pair(fNode->right, lvl+1));
-        }
-        
-        for(auto val : m)
-        {
-            ans.push_back(val.second);
-        }
+        solve(root, ans, 0);
         
         return ans;
+        
     }
+
+
+
+    // vector<int> leftView(Node *root) {
+    //     // code here
+    //     vector<int> ans;
+    //     if(!root)
+    //     return ans;
+        
+    //     map<int, int> m;
+    //     queue<pair<Node*, int>> q;
+    //     q.push(make_pair(root, 0));
+        
+    //     while(!q.empty())
+    //     {
+    //         pair<Node*, int> temp = q.front();
+    //         q.pop();
+            
+    //         Node* fNode = temp.first;
+    //         int lvl = temp.second;
+            
+    //         if(m.find(lvl) == m.end())
+    //         m[lvl] = fNode -> data;
+            
+    //         if(fNode -> left)
+    //         q.push(make_pair(fNode->left, lvl+1));
+            
+    //         if(fNode -> right)
+    //         q.push(make_pair(fNode->right, lvl+1));
+    //     }
+        
+    //     for(auto val : m)
+    //     {
+    //         ans.push_back(val.second);
+    //     }
+        
+    //     return ans;
+    // }
 };
